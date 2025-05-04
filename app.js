a094cfcf1c2a43defea167ec20a3b02bc838e3d6
usa3amustafa <usa3a3ustafa@outlook.com>
2025-05-04 23:52:48 +0500

    mobile smooth scrolling

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 let smoother = ScrollSmoother.create({
   wrapper: '#smooth-wrapper',
   content: '#smooth-content',
-  smooth: 1,
+  smooth: 0.75,
+  smoothTouch: 0.025,
 })
