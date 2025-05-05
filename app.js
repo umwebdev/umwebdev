fb0ffdff101ba2f9fe7374a3a11affe97ee47191
usa3amustafa <usa3a3ustafa@outlook.com>
2025-05-05 23:51:30 +0500

    update

--- app.js
+++ app.js
@@ -17,21 +17,22 @@ window.addEventListener('load', () => {
   // reveal the container immediately (before any frames)
   gsap.set('.header-title', { visibility: 'visible' })
   // set your from-values right away
-  gsap.set(split.words, { autoAlpha: 0, y: 20 })
+  // gsap.set(split.words, { autoAlpha: 0, y: 20 })
+
+  // .to(split.words, {
+  //   autoAlpha: 1,
+  //   y: 0,
+  //   stagger: 0.2,
+  //   ease: 'back.out(1.7)',
+  // })
 
   // now your timeline just animates up to the natural state
   const tl = gsap.timeline()
-  tl.to(split.words, {
-    autoAlpha: 1,
-    y: 0,
-    stagger: 0.2,
-    ease: 'back.out(1.7)',
-  })
-    .from(
-      '.stat',
-      { autoAlpha: 0, y: 20, stagger: 0.1, ease: 'back.out(1.7)' },
-      '-=0.25'
-    )
+  tl.from(
+    '.stat',
+    { autoAlpha: 0, y: 20, stagger: 0.1, ease: 'back.out(1.7)' },
+    '-=0.25'
+  )
     .from(
       '.skill',
       { autoAlpha: 0, y: 10, stagger: 0.1, ease: 'back.out(1.7)' },
