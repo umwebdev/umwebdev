5d72731556ca7419df750fc3e5439c46924f5026
usa3amustafa <usa3a3ustafa@outlook.com>
2025-05-05 22:23:13 +0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText)
 
 let smoother = ScrollSmoother.create({
   wrapper: '#smooth-wrapper',
@@ -6,3 +6,42 @@ let smoother = ScrollSmoother.create({
   smooth: 1,
   smoothTouch: 0.1,
 })
+
+let split = new SplitText('.header-title', { type: 'words' })
+
+console.log(split.chars)
+
+const tl = gsap.timeline()
+
+tl.from(split.words, {
+  opacity: 0,
+  y: 10,
+  stagger: 0.2,
+  ease: 'back',
+})
+  .from(
+    '.stat',
+    {
+      opacity: 0,
+      stagger: 0.1,
+      y: 20,
+      ease: 'back',
+    },
+    '-=0.25'
+  )
+  .from(
+    '.skill',
+    {
+      opacity: 0,
+      y: 10,
+      stagger: 0.1,
+      ease: 'back',
+    },
+    '-=0.25'
+  )
+  .from('.logo', {
+    opacity: 0,
+    y: '-100px',
+    ease: 'back',
+  })
+  .from('.nav-cta', { opacity: 0, right: '-100%' }, '-=0.25')
