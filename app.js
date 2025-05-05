b3fc1c20ade25d28db2eb9524e8b270623b3c6f3
usa3amustafa <usa3a3ustafa@outlook.com>
2025-05-05 23:53:14 +0500

    update

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ window.addEventListener('load', () => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText)
 
   // create smoother
-  // ScrollSmoother.create({
-  //   wrapper: '#smooth-wrapper',
-  //   content: '#smooth-content',
-  //   smooth: 1,
-  //   smoothTouch: 0.1,
-  // })
+  ScrollSmoother.create({
+    wrapper: '#smooth-wrapper',
+    content: '#smooth-content',
+    smooth: 1,
+    smoothTouch: 0.1,
+  })
 
   // ScrollTrigger.refresh()
 
@@ -19,20 +19,19 @@ window.addEventListener('load', () => {
   // set your from-values right away
   // gsap.set(split.words, { autoAlpha: 0, y: 20 })
 
-  // .to(split.words, {
-  //   autoAlpha: 1,
-  //   y: 0,
-  //   stagger: 0.2,
-  //   ease: 'back.out(1.7)',
-  // })
-
   // now your timeline just animates up to the natural state
   const tl = gsap.timeline()
-  tl.from(
-    '.stat',
-    { autoAlpha: 0, y: 20, stagger: 0.1, ease: 'back.out(1.7)' },
-    '-=0.25'
-  )
+  tl.to(split.words, {
+    autoAlpha: 1,
+    y: 0,
+    stagger: 0.2,
+    ease: 'back.out(1.7)',
+  })
+    .from(
+      '.stat',
+      { autoAlpha: 0, y: 20, stagger: 0.1, ease: 'back.out(1.7)' },
+      '-=0.25'
+    )
     .from(
       '.skill',
       { autoAlpha: 0, y: 10, stagger: 0.1, ease: 'back.out(1.7)' },
